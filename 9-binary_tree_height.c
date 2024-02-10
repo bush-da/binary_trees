71047cbf743f5252c379f3612f9722e50770db1b
Daniel Tujuma <danielsevani12@gmail.com>
2024-02-10 04:23:43 -0500

    fix minor issue

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
 
+		size_t left, right;
 
-	size_t left, right;
+		left = 0;
+		right = 0;
 
-	left = 0;
-	right = 0;
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return (left > right ? left : right);
+		return (left > right ? left : right);
+	}
+	return (0);
 }
